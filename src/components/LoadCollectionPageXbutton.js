bfb6ae74ec0fd62d433eec8822f2438c89c44f27
Kenneth Lyerly <kenput3r@gmail.com>
2019-07-28 20:48:17 -0700

    paging working with pareInt

--- src/components/LoadCollectionPageXbutton.js
+++ src/components/LoadCollectionPageXbutton.js
@@ -6,15 +6,44 @@ export default class LoadCollectionPageXbutton extends BaseClass {
     super(rootElement, args);
 
     this.collection = document.querySelector('[data-collection]').dataset;
+    this.starting_page = parseInt(this.collection.currentPage);
+    this.ending_page = this.starting_page === 1 ? parseInt(this.collection.pages) : this.starting_page - 1;
+    this.state_object = {};
 
     this.init();
   }
 
   init() {
-    this.rootElement.addEventListener('click', () => {
-      console.log(this.collection);
-      console.log(this.collection.currentPage);
-      getCollectionPageX(this.collection.url, 2);
+    this.rootElement.addEventListener('click', async () => {
+      let next_page;
+
+      if(this.starting_page === 1 && parseInt(this.collection.currentPage) !== this.ending_page) {
+        next_page = parseInt(this.collection.currentPage) + 1;
+      }else if(this.starting_page !== 1 && parseInt(this.collection.currentPage) !== this.ending_page) {
+        if(parseInt(this.collection.currentPage) === parseInt(this.collection.pages)) {
+          next_page = 1;
+        }else{
+          next_page = parseInt(this.collection.currentPage) + 1;
+        }
+      }else{
+        return;
+      }
+
+      let page_title = this.collection.pageTitle+' | Page '+next_page;
+      let collection_url = 'https://'+this.collection.url+'?view=products&page='+next_page;
+
+      try {
+        let elements = await new Promise((resolve, reject)=> getCollectionPageX(collection_url, {resolve: resolve, reject: reject}));
+        document.getElementById('Products').insertAdjacentHTML('beforeend', elements);
+        history.pushState(this.state_object, page_title, collection_url.replace('view=products&', ''));
+        this.collection.currentPage = next_page;
+        if(parseInt(this.collection.currentPage) === parseInt(this.ending_page)) {
+          this.rootElement.classList.add('hide');
+        }
+      }catch(error) {
+        console.log(error);
+      }
+
     });
   }
 }
--- src/utils/getCollectionPage.js
+++ src/utils/getCollectionPage.js
@@ -1,11 +1,15 @@
-async function getCollectionPage(url, page) {
+/**
+ * @method getCollectionPage
+ * @param {String} url The url of the collection to fetch
+ * @param {object} promise Used for resolving or rejecting a Promise
+ */
+async function getCollectionPage(url, promise) {
   const headers = new Headers({'Content-Type': 'text/html'});
-  const collection_url = 'https://'+url;
-  console.log(url);
-  const new_markup = await fetch(collection_url, {headers: headers}).then((res)=> {
-    return res.text();
-  }).then((data)=> {
-    console.log(data);
-  });
+
+  const collection_products = await fetch(url, {headers: headers});
+  if(collection_products.status === 404) return promise.reject({error: 404});
+
+  const elements = await collection_products.text();
+  return promise.resolve(elements);
 }
 export default getCollectionPage;
