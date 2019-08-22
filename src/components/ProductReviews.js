51982de5f2a6f01d171c9cd6ecb3f64d08aea804
Kenneth Lyerly <kenput3r@gmail.com>
2019-08-21 21:15:11 -0700

    partial support for loading more reviews

--- src/components/ProductReviews.js
+++ src/components/ProductReviews.js
@@ -6,6 +6,7 @@ export default class ProductReviews extends BaseClass {
     this.app_key = 'q7EYKyLuCq4wDQ7s0M36GkawfE2I4JDL9gg4wvGY';
     this.getReviews = this.getReviews.bind(this);
     this.generateStars = this.generateStars.bind(this);
+    this.vote = this.vote.bind(this);
     this.generateCard = this.generateCard.bind(this);
     this.displayReviews = this.displayReviews.bind(this);
     this.init();
@@ -27,22 +28,25 @@ export default class ProductReviews extends BaseClass {
     const empty_stars = 5 - half_stars - full_stars;
     const star_container = document.createElement('SPAN');
     for(let i = 1; i <= score; i++) {
-      star_container.insertAdjacentHTML('beforeend', '<i class="material-icons tiny">star</i>');
+      star_container.insertAdjacentHTML('beforeend', '<i class="material-icons">star</i>');
     }
-    half_stars === 1 ? star_container.insertAdjacentHTML('beforeend', '<i class="material-icons tiny">star_half</i>') : null;
+    half_stars === 1 ? star_container.insertAdjacentHTML('beforeend', '<i class="material-icons">star_half</i>') : null;
     for(let i = 1; i <= empty_stars; i++) {
-      star_container.insertAdjacentHTML('beforeend', '<i class="material-icons tiny">star_border</i>');
+      star_container.insertAdjacentHTML('beforeend', '<i class="material-icons">star_border</i>');
     }
     return star_container;
   }
 
   async vote() {
-    const thumb = event.target.dataset.vote;
-    const review_id = event.target.parentElement.dataset.id;
+    const el = event.target;
+    const thumb = el.dataset.vote;
+    const review_id = el.parentElement.dataset.id;
     const url = `https://api.yotpo.com/reviews/${review_id}/vote/${thumb}`;
     const headers = new Headers({'content-type': 'application/json'});
     const response = await fetch(url, {type: 'POST', headers: headers});
     const results = await response.json();
+    console.log(review_id);
+    el.removeEventListener('click', this.vote);
     console.log(results);
   }
 
@@ -70,6 +74,21 @@ export default class ProductReviews extends BaseClass {
     return card;
   }
 
+  viewMoreButton(next_page) {
+    const button = document.createElement('DIV');
+    button.classList.add('row', 'button-container');
+    button.insertAdjacentHTML('beforeend', `<div class="row button-container">
+      <div class="col s12 center-align">
+        <button class="btn waves-effect waves-light background-s-red">View More</button>
+      </div>
+    </div>`);
+    button.querySelector('.btn').addEventListener('click', async()=> {
+      const reviews = await new Promise((resolve, reject)=>this.getReviews({resolve:resolve, reject:reject}, next_page));
+      this.displayReviews(reviews);
+    })
+    this.rootElement.appendChild(button);
+  }
+
   displayReviews(yotpo) {
     const total_reviews = yotpo.bottomline.total_review;
     const total_pages = Math.ceil(total_reviews / yotpo.pagination.per_page);
@@ -85,6 +104,11 @@ export default class ProductReviews extends BaseClass {
     }
     if(current_page < total_pages) {
       console.log(`There are ${total_pages - current_page} pages remaining`);
+      const next_page = current_page + 1;
+      const old_button = this.rootElement.querySelector('.button-container');
+      console.log(old_button);
+      if(old_button) this.rootElement.removeChild(old_button);
+      this.viewMoreButton(next_page);
     }
   }
 
