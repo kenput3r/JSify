6a623813807d80843a59402098912de63b7ea109
Kenneth Lyerly <kenput3r@gmail.com>
2019-09-08 10:08:10 -0700

    fix nav searchbar losing focus

--- src/components/DesktopNav.js
+++ src/components/DesktopNav.js
@@ -8,6 +8,7 @@ export default class DesktopNav extends BaseClass {
   constructor(rootElement, args) {
     super(rootElement, args);
     this.active_trigger;
+    this.search_hold_focus;
     this.init();
   }
 
@@ -27,7 +28,30 @@ export default class DesktopNav extends BaseClass {
     trigger.classList.remove('active');
   }
 
+  /**
+   * @method searchRefocus - Fixes search losing focus when dropdown closes.
+   * Toggles search icon boldness.
+   */
+  searchRefocus() {
+    this.rootElement.querySelector('.search-bar').addEventListener('focusin', (event) => {
+      this.search_hold_focus = true;
+      setTimeout(() => {
+        this.search_hold_focus = false;
+      }, 150);
+      this.rootElement.querySelector('li.search-icon i').classList.add('bold');
+    });
+
+    this.rootElement.querySelector('.search-bar').addEventListener('focusout', (event) => {
+      this.rootElement.querySelector('li.search-icon i').classList.remove('bold');
+      if(this.search_hold_focus) {
+        this.rootElement.querySelector('.aa-input').focus();
+        this.rootElement.querySelector('li.search-icon i').classList.add('bold');
+      }
+    });
+  }
+
   init() {
+    this.searchRefocus();
     const triggers = this.rootElement.querySelectorAll('.dropdown-trigger');
     const dropdown_options = {
       constrainWidth: false,
