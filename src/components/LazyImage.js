c622547569aa4b455f8a8f9fd5265fc6b591cd8e
j-Riv <jose.alfredo.rivera@gmail.com>
2019-10-27 16:05:27 -0700

    Fixed: Lazy Image component

--- src/components/LazyImage.js
+++ src/components/LazyImage.js
@@ -7,40 +7,36 @@ import BaseClass from '../system/BaseClass';
 export default class LazyImage extends BaseClass {
     constructor(rootElement, args) {
         super(rootElement, args);
+        this.active = false;
         this.lazyLoad = this.lazyLoad.bind(this);
         this.init();
     }
 
     lazyLoad() {
         if (this.active === false) {
-            this.active = true;
             const lazyImage = this.rootElement;
-            setTimeout(function () {
-                if ((lazyImage.getBoundingClientRect().top <= window.innerHeight 
+            setTimeout(() => {
+                if ((lazyImage.getBoundingClientRect().top <= window.innerHeight
                     && lazyImage.getBoundingClientRect().bottom >= 0)
                     && getComputedStyle(lazyImage).display !== "none") {
-
                     let poster = lazyImage.classList.contains('lazy-poster');
                     if (poster) {
-                        lazyImage.style.backgroundImage = 'url(' + lazyImage.dataset.src + ')'; 
+                        lazyImage.style.backgroundImage = 'url(' + lazyImage.dataset.src + ')';
                     } else {
                         lazyImage.src = lazyImage.dataset.src;
                     }
 
                     lazyImage.classList.remove("lazy-image");
-
+                    this.active = true;
                     document.removeEventListener("scroll", this.lazyLoad);
-                    window.removeEventListener("resize",this.lazyLoad);
-                    window.removeEventListener("orientationchange",this.lazyLoad);
-
+                    window.removeEventListener("resize", this.lazyLoad);
+                    window.removeEventListener("orientationchange", this.lazyLoad);   
                 }
-                this.active = false;
             }, 200);
         }
     }
 
     init() {
-        this.active = false;
         document.addEventListener("scroll", this.lazyLoad);
         window.addEventListener("resize", this.lazyLoad);
         window.addEventListener("orientationchange", this.lazyLoad);
