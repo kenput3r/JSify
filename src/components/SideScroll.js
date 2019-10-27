80205f7b0ff7f8b6df10408c1ed6b1f12f9e5de6
kenput3r <kenput3r@gmail.com>
2019-10-27 08:20:18 -0700

    support for pre-loader

--- src/components/SideScroll.js
+++ src/components/SideScroll.js
@@ -8,13 +8,37 @@ export default class SideScroll extends BaseClass {
     this.last_tile;
     this.loading = false;
     this.page = 1;
-
+    this.toggleLoader = this.toggleLoader.bind(this);
     this.init();
   }
 
   getLastTile() {
     const tiles = this.rootElement.querySelectorAll('.product-tile');
     return tiles[tiles.length - 1];
+  }
+
+  toggleLoader() {
+    if(this.loading) {
+      const height = this.rootElement.querySelector('.side-scroll-card').offsetHeight;
+      const width = this.rootElement.querySelector('.side-scroll-card').offsetWidth;
+      const loader = `<div class="side-scroll-card pre-loader-card valign-wrapper" style="width:${width}px; height:${height}px">
+      <div class="preloader-wrapper active" style="margin-left:auto; margin-right:auto">
+        <div class="spinner-layer spinner-red-only">
+          <div class="circle-clipper left">
+            <div class="circle"></div>
+          </div><div class="gap-patch">
+            <div class="circle"></div>
+          </div><div class="circle-clipper right">
+            <div class="circle"></div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    this.rootElement.insertAdjacentHTML('beforeend', loader);
+    }else{
+      const loader = this.rootElement.querySelector('.pre-loader-card');
+      loader.parentNode.removeChild(loader);
+    }
   }
 
   init() {
@@ -25,6 +49,7 @@ export default class SideScroll extends BaseClass {
         const scroll_left = event.target.scrollLeft;
         if(this.last_tile.offsetLeft - window.innerWidth - scroll_left <= 600 && !this.loading) {
           this.loading = true;
+          this.toggleLoader();
           const elements = await new Promise((resolve, reject) => getCollectionPage(`${url}${this.page}`, {resolve: resolve, reject: reject}));
           this.rootElement.insertAdjacentHTML('beforeend', elements);
           const new_tiles = document.querySelectorAll(`.product-tile-page-${this.page}`);
@@ -33,6 +58,7 @@ export default class SideScroll extends BaseClass {
           }
           this.page = this.page + 1;
           this.loading = false;
+          this.toggleLoader();
           this.last_tile = this.getLastTile();
         }
       }
