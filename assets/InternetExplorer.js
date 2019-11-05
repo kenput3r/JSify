fe1235e9bbb2190c83173b153549effeae430c9d
kenput3r <kenput3r@gmail.com>
2019-11-04 17:39:48 -0800

    internet explorer page

---
+++ assets/InternetExplorer.js
@@ -0,0 +1,11 @@
+function isIE() {
+  ua = navigator.userAgent;
+  var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+  return is_ie; 
+}
+
+if (isIE()){
+  window.location = 'https://suavecito.com/pages/internet-explorer?view=InternetExplorer';
+}else{
+  window.location = 'https://suavecito.com/pages/internet-explorer?view=InternetExplorer';
+}
